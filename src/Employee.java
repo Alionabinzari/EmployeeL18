1b06e5793fd1b5213d1fdea4bb07fb0b63ac4319
aliona.binzari <alionab79@gmail.com>
2023-11-20 14:36:45 +0200

    Tema pacasa l.18

--- src/Employee.java
+++ src/Employee.java
@@ -1,16 +1,43 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Employee {
-    private boolean isPazedPerHour;
+    private boolean isPayedPerHour;
     int age;
 
-    public Employee(boolean isPazedPerHour, int age) {
-        this.isPazedPerHour = isPazedPerHour;
+    public Employee(boolean isPayedPerHour, int age) {
+        this.isPayedPerHour = isPayedPerHour;
         this.age = age;
     }
 
-    List<Employee> employeeList = Arrays.asList();
-    employeeList.stream().filter(e->e.getAge()>25 &&e.isPayedPerHour() &&
-    e.isPayedPerHour() e.getDepartment ().equals ("Management")).distinct () .limit(5) .forEach(System.out::println);
+    public boolean isPazedPerHour() {
+        return isPayedPerHour;
+    }
 
+    public void setPazedPerHour(boolean isPayedPerHour) {
+        isPayedPerHour = isPayedPerHour;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Employee() {
+        super();
+    }
+
+
+    public boolean isPayedPerHour()
+    {
+        return  isPayedPerHour();
+    }
+
+    public Object getDepartment() {
+        return getDepartment();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Employee> employeeList  = Arrays.asList();
+
+        employeeList.stream()
+                .filter(e -> e.getAge()>25 &&
+                        e.isPayedPerHour() &&
+                        e.getDepartment().equals ("Management"))
+                .distinct ()
+                .limit(5)
+                .forEach(System.out::println);
+
     }
 }
